750eb04d3ecc6102958c83e40beeb3dfab02bce4
MawiraIke <mawiraike@gmail.com>
2019-09-12 21:56:35 +0300

    Solution one

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,12 +1,10 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(def start-pos [[:fox :goose :corn :you] [:boat] []])       ;:you
+(def start-pos [[:fox :goose :corn] [:boat] []])            ;:you
 
 (def current-pos (atom start-pos))
 
 (def end-pos [[] [:boat] [:fox :goose :corn :you]])
-
-(def boat-capacity 3)
 
 (defn check-friendliness
   "Check whether two items are friendly, e.g. The fox and the goose are not"
@@ -26,190 +24,151 @@
   [move]
   (= end-pos move))
 
-(defn perform-move
-  "This move is called to perform a move"
-  ([elem] (let [remove-element (fn [init-vec]
-                                 ;Thanks to https://stackoverflow.com/questions/57591009/converting-a-set-to-a-vector-results-in-a-vector-of-nested-vectors-in-clojure/57591310#57591310
-                                 (mapv vec (map #(disj (set %) elem :you) init-vec)))
-                do-swap (fn [new-vec]
-                          (reset! current-pos new-vec))
-                add-elem (fn [new-vec]
-                           (assoc new-vec 2 (conj (get new-vec 2) elem :you)))
-                ;Print >>  [[:goose] [:boat] [:fox :corn :you]]
-                remove-user (fn [init-vec]
-                              (mapv vec (map #(disj (set %) :you) init-vec)))
-                return-user (fn [init-vec]
-                              (assoc init-vec 0 (conj (get init-vec 0) :you)))]
-            (->> @current-pos
-                 (remove-element)
-                 (do-swap)
-                 (add-elem)
-                 (do-swap)
-                 (remove-user)
-                 (return-user)
-                 (do-swap))))
-  ([elem-1 elem-2] (let [remove-element (fn [init-vec]
-                                          (mapv vec (map #(disj (set %) elem-1 elem-2 :you) init-vec)))
-                         do-swap (fn [new-vec]
-                                   (reset! current-pos new-vec))
-                         add-elem (fn [new-vec]
-                                    (assoc new-vec 2 (conj (get new-vec 2) elem-1 elem-2 :you)))
-                         ;Print >>  [[:goose] [:boat] [:fox :corn :you]]
-                         remove-user (fn [init-vec]
-                                       (mapv vec (map #(disj (set %) :you) init-vec)))
-                         return-user (fn [init-vec]
-                                       (assoc init-vec 0 (conj (get init-vec 0) :you)))]
-                     (->> @current-pos
-                          (remove-element)
-                          (do-swap)
-                          (add-elem)
-                          (do-swap)
-                          (remove-user)
-                          (return-user)
-                          (do-swap)))))
+(defn three-item-logic [elements]
+  "Function that receives three characters
+  vector and finds which to move"
+  (let [elem-1 (get elements 0)
+        elem-2 (get elements 1)
+        elem-3 (get elements 2)]
+    (if (check-friendliness elem-1 elem-2)
+      elem-3
+      (if (check-friendliness elem-1 elem-3)
+        elem-2
+        (if (check-friendliness elem-2 elem-3)
+          elem-1
+          :List-not-supported)))))
 
-(defn perform-move2
-  "This move is called to perform a move"
-  ([elem initial-vector] (let [remove-element (fn [init-vec]
-                                                ;Thanks to https://stackoverflow.com/questions/57591009/converting-a-set-to-a-vector-results-in-a-vector-of-nested-vectors-in-clojure/57591310#57591310
-                                                (mapv vec (map #(disj (set %) elem :you) init-vec)))
-                               add-elem (fn [new-vec]
-                                          (assoc new-vec 2 (conj (get new-vec 2) elem :you)))
-                               ;Print >>  [[:goose] [:boat] [:fox :corn :you]]
-                               remove-user (fn [init-vec]
-                                             (mapv vec (map #(disj (set %) :you) init-vec)))
-                               return-user (fn [init-vec]
-                                             (assoc init-vec 0 (conj (get init-vec 0) :you)))]
-                           (->> initial-vector
+(defn get-index [data item]
+  (let [n (count (first data))
+        i (.indexOf (flatten data) item)]
+    (if (pos? i)
+      (list (quot i n) (mod i n)))))
+
+(defn river-crossing-plan [current-state]
+  "Function that receives two characters
+  vector and finds which to move"
+  (let [side-b (fn [current-state]
+                 (let [current-state @current-pos]
+                   (if (empty? (get current-state 2))
+                     (let [elem-1 (get-in current-state [1 1])
+                           remove-element (fn [init-vec]
+                                            (mapv vec (map #(disj (set %) elem-1 :you) init-vec)))
+                           add-elem (fn [new-vec]
+                                      (assoc new-vec 2 (conj (get new-vec 2) elem-1 :you)))
+                           perform-swap (fn [new-vec]
+                                          (reset! current-pos new-vec))]
+
+                       (->> current-state
+                            (remove-element)
+                            (add-elem)
+                            (perform-swap)
+                            (print @current-pos))
+                       ())
+                     (let [elem-1 (get-in current-state [1 1])
+                           elem-2 (get-in current-state [2 0])
+                           remove-element (fn [init-vec]
+                                            (mapv vec (map #(disj (set %) elem-1 :you) init-vec)))
+                           add-elem (fn [new-vec]
+                                      (assoc new-vec 2 (conj (get new-vec 2) elem-1 :you)))
+                           perform-swap (fn [new-vec]
+                                          (reset! current-pos new-vec))
+                           remove-element-2 (fn [init-vec]
+                                              (mapv vec (map #(disj (set %) elem-2) init-vec)))
+                           add-elem-2 (fn [new-vec]
+                                        (assoc new-vec 1 (conj (get new-vec 1) elem-2)))]
+
+                       (if (= 2 (.length (get @current-pos 2)))
+                         (->> current-state
+                              (remove-element)
+                              (add-elem)
+                              (perform-swap)
+                              (print @current-pos))
+                         (if (check-friendliness elem-1 elem-2)
+                           (->> current-state
                                 (remove-element)
                                 (add-elem)
-                                (remove-user)
-                                (return-user))))
-  ([elem-1 elem-2 initial-vector] (let [remove-element (fn [init-vec]
-                                                         (mapv vec (map #(disj (set %) elem-1 elem-2 :you) init-vec)))
-                                        add-elem (fn [new-vec]
-                                                   (assoc new-vec 2 (conj (get new-vec 2) elem-1 elem-2 :you)))
-                                        ;Print >>  [[:goose] [:boat] [:fox :corn :you]]
-                                        remove-user (fn [init-vec]
-                                                      (mapv vec (map #(disj (set %) :you) init-vec)))
-                                        return-user (fn [init-vec]
-                                                      (assoc init-vec 0 (conj (get init-vec 0) :you)))]
-                                    (->> initial-vector
-                                         (remove-element)
-                                         (add-elem)
-                                         (remove-user)
-                                         (return-user)))))
+                                (perform-swap)
+                                (print @current-pos))
+                           (->> current-state
+                                (remove-element)
+                                (add-elem)
+                                (remove-element-2)
+                                (add-elem-2)
+                                (perform-swap)
+                                (print @current-pos))))))))
 
-;TODO CHECK HOW TO RECUR THIS
-;Should work fine for method one of solution
-;This is where only one formulae is applied,
-;It should fetch all the ways and show them and show method 1 as the fastest formulae
-(defn river-crossing-plan [new-vector]                      ;(river-crossing-plan @current-pos)
-  (loop [iv new-vector]
-    (let [side-a (get iv 0)                                 ;Initial side
-          side-b (get iv 1)                                 ;Boat
-          side-c (get iv 2)]                                ;End side
-      (cond
-        (final-move? current-pos)
-        (println "Game-over, you successfully moved all the items!")
+        perform-swap (fn [new-vec]
+                       (reset! current-pos new-vec))
+        side-a (fn [current-state]
+                 (let [current-state @current-pos]
+                   (cond
+                     (= 3 (.length (get current-state 0)))
+                     (let [elem-1 (three-item-logic [(get-in current-state [0 0]) (get-in current-state [0 1]) (get-in current-state [0 2])])
+                           remove-element (fn [init-vec]
+                                            ;Thanks to https://stackoverflow.com/questions/57591009/converting-a-set-to-a-vector-results-in-a-vector-of-nested-vectors-in-clojure/57591310#57591310
+                                            (mapv vec (map #(disj (set %) elem-1 :you) init-vec)))
+                           add-element (fn [new-vec]
+                                         (assoc new-vec 1 (conj (get new-vec 1) elem-1)))]
+                       (->> current-state
+                            (remove-element)
+                            (add-element)
+                            (perform-swap)
+                            ;Call side B here
+                            (print @current-pos)))
 
-        :else
-        (case (.length side-a)
-          0 (print "No more items to move")
-          1 (let [elem-1 (get side-a 0)]
-              (perform-move elem-1))
-          2 (let [elem-1 (get side-a 0)
-                  elem-2 (get side-a 1)]
-              (if [= (check-friendliness elem-1 elem-2) :true]
-                (recur (perform-move elem-1 elem-2))
-                (println "Unmovable items")))
-          3 (let [elem-1 (get side-a 0)
-                  elem-2 (get side-a 1)
-                  elem-3 (get side-a 2)]
-              (if (check-friendliness elem-1 elem-2)
-                (recur (perform-move elem-1 elem-2))
-                (if (check-friendliness elem-1 elem-3)
-                  (recur (perform-move elem-1 elem-3))
-                  (if (check-friendliness elem-2 elem-3)
-                    (recur (perform-move elem-2 elem-3))
-                    (println "Unmovable items")))))
-          4 (let [remove-element (fn [init-vec]
-                                   ;Thanks to https://stackoverflow.com/questions/57591009/converting-a-set-to-a-vector-results-in-a-vector-of-nested-vectors-in-clojure/57591310#57591310
-                                   (mapv vec (map #(disj (set %) :you) init-vec)))
-                  swap-elements (fn [vec1]
-                                  (reset! current-pos vec1))]
-              (->> @current-pos
-                   (remove-element)
-                   (swap-elements)
-                   (let [elem-1 (get side-a 0)
-                         elem-2 (get side-a 1)
-                         elem-3 (get side-a 2)]
-                     (if (check-friendliness elem-1 elem-2)
-                       (do
-                         (recur (perform-move elem-1 elem-2)))
-                       (if (check-friendliness elem-1 elem-3)
-                         (do
-                           (recur (perform-move elem-1 elem-3)))
-                         (if (check-friendliness elem-2 elem-3)
-                           (do
-                             (recur (perform-move elem-2 elem-3))) ())))))))))))
+                     (= 2 (.length (get current-state 0)))
+                     (let [elem-to-be-moved (get-in @current-pos [0 0])
+                           remove-element (fn [elem-1]
+                                            (mapv vec (map #(disj (set %) elem-to-be-moved :you) @current-pos)))
+                           add-element (fn [new-vec]
+                                         (assoc new-vec 1 (conj (get new-vec 1) elem-to-be-moved)))]
 
-(defmacro do-until [& clauses]
-  (when clauses
-    (list `when (first clauses)
-          (if (next clauses)
-            (second clauses)
-            (throw (IllegalArgumentException.
-                     "do-until requires an even number of forms")))
-          (cons 'do-until (nnext clauses)))))
+                       (->> current-state
+                            (remove-element)
+                            (add-element)
+                            (perform-swap)
+                            ;Call side B here
+                            (print @current-pos)))
 
-;
-;Get vector
-;loop through elements, one each time
+                     (= 1 (.length (get current-state 0)))
+                     (let [elem-to-be-moved (get-in @current-pos [1 1])
+                           length (get @current-pos 1)
+                           elem-to-be-moved-2 (get-in @current-pos [0 0])
+                           remove-element (fn [new-vec]
+                                            (mapv vec (map #(disj (set %) elem-to-be-moved :you) new-vec)))
+                           add-element (fn [new-vec]
+                                         (assoc new-vec 0 (conj (get new-vec 0) elem-to-be-moved :you)))
+                           remove-element-2 (fn [new-vec]
+                                              (mapv vec (map #(disj (set %) elem-to-be-moved-2 :you) new-vec)))
+                           add-element-2 (fn [new-vec]
+                                           (assoc new-vec 1 (conj (get new-vec 1) elem-to-be-moved-2)))]
 
-(defn river-crossing-plan2 []
-  (let [side-a (get @current-pos 0)
-        side-b (get @current-pos 1)
-        side-c (get @current-pos 2)
-        swap-elements (fn [vec1]
-                        (reset! current-pos vec1))]
+                       (if (< (.length length) 2)
+                         (->> current-state
+                              (remove-element-2)
+                              (add-element-2)
+                              (perform-swap)
+                              (print @current-pos))         ;Last round of moves
+                         (->> current-state
+                              (remove-element)
+                              (add-element)
+                              (remove-element-2)
+                              (add-element-2)
+                              (perform-swap)
+                              (print @current-pos))))       ;First time you come to side A after leaving
 
-    (do-until
-      (= 4 (.length (get @current-pos 0))) (->> @current-pos
-                                                ;(remove-element)
-                                                (swap-elements)
-                                                (let [elem-1 (get side-a 0)
-                                                      elem-2 (get side-a 1)
-                                                      elem-3 (get side-a 2)]
-                                                  (if (check-friendliness elem-1 elem-2)
-                                                    (perform-move elem-1 elem-2)
-                                                    (if (check-friendliness elem-1 elem-3)
-                                                      (perform-move elem-1 elem-3)
-                                                      (if (check-friendliness elem-2 elem-3)
-                                                        (perform-move elem-2 elem-3)
-                                                        (println "Unmovable items"))))))
-      (= 3 (.length (get @current-pos 0))) (->> @current-pos
-                                                (let [elem-1 (get (get @current-pos 0) 0)
-                                                      elem-2 (get (get @current-pos 0) 1)
-                                                      elem-3 (get (get @current-pos 0) 2)]
-                                                  (if (check-friendliness elem-1 elem-2)
-                                                    (perform-move elem-1 elem-2)
-                                                    (if (check-friendliness elem-1 elem-3)
-                                                      (perform-move elem-1 elem-3)
-                                                      (if (check-friendliness elem-2 elem-3)
-                                                        (perform-move elem-2 elem-3)
-                                                        (println "Unmovable items")))))
-                                                (swap-elements))
-      (= 2 (.length (get @current-pos 0))) (@current-pos
-                                             (let [elem-1 (get (get @current-pos 0) 0)
-                                                   elem-2 (get (get @current-pos 0) 1)]
-                                               (if [= (check-friendliness elem-1 elem-2) :true]
-                                                 (perform-move elem-1 elem-2)
-                                                 ()))
-                                             (swap-elements))
-      (= 1 (.length (get @current-pos 0))) (let [elem-1 (get (get @current-pos 0) 0)]
-                                             (perform-move elem-1))
-      (= 0 (.length (get @current-pos 0))) (print (str "Game over, you have finally moved all items" @current-pos))
-      :lollipop (println "Truthy thing"))))
+                     :else
+                     (print "End of sequence"))))]
+
+    (cond (= start-pos current-state)
+          (->> current-state
+               (side-a)                                     ;Does not continue because the print statements in side A print null
+               (side-b)
+               (side-a)
+               (side-b)
+               (side-a)
+               (side-b)
+               (side-a)
+               (side-b)))))
 
 ;163
