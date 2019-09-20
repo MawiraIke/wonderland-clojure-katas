f44d19dd9d875ed8e845e4f3a0e662a4263f1075
MawiraIke <mawiraike@gmail.com>
2019-09-20 11:11:03 +0300

    Replace function

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -5,9 +5,9 @@
              [2.5 3.0 3.5]
              [4.0 4.5 5.0]])
 
-(def current-values (atom [[0 0 0]
-                           [0 0 0]
-                           [0 0 0]]))
+(def current-values (atom [[nil nil nil]
+                           [nil nil nil]
+                           [nil nil nil]]))
 
 (defn magic-square [values]
   [[1.0 1.5 2.0]
@@ -37,15 +37,31 @@
     0))
 
 (defn add-logic [current-loc]
+  "Expects a location vector, returns the next space to fill up"
   (let [num-a (get current-loc 0)
         num-b (get current-loc 1)]
     [(add-a num-a) (add-b num-b)]))
 
-(defn add-elem-1 [values]
+(defn replace-func
+  "Replaces the first vector middle location in a 3*3 matrix.
+  This is the first step to solving a 3*3 matrix"
+  ([values] (into [] (cons (get-in values [0 0]) (conj [1.0] (get-in values [0 2])))))
+  ([[k v] r] (cond
+               (= v 0)
+               (into [] (cons r (conj (get-in values [k 1]) (get-in values [k 2]))))
+               (= v 1)
+               (into [] (cons (get-in values [k 0]) (conj [r] (get-in values [2]))))
+               (= v 2)
+               (into [] (cons (get-in values [k 0]) (conj (get-in values [k 1]) [r]))))))
+
+(defn add-elems [values]
   (cond
     (= 3 (.length values))
     (let [n (get-in values [0 1])]
-      (replace {n 1.0} (get values 0)))
+      ;(replace {n 1.0} (get values 0))
+      (do
+        (replace-func (get values 0))
+        (replace-func (add-logic [0 1]) 2)))
 
     :else
     (println "Operations only supports 3*3 matrices")))
