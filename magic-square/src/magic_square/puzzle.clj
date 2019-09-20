9adf24ae9b45f88675fbdd0f309d536c320bbb46
MawiraIke <mawiraike@gmail.com>
2019-09-20 13:34:11 +0300

    Continue here, stuck at loop three

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -48,23 +48,37 @@
   ([values] (into [] (cons (get-in values [0 0]) (conj [1.0] (get-in values [0 2])))))
   ([[k v] r] (cond
                (= v 0)
-               (into [] (cons r (conj (get-in values [k 1]) (get-in values [k 2]))))
+               (into [] (cons r (conj [(get-in @current-values [k 1])] (get-in @current-values [k 2]))))
                (= v 1)
-               (into [] (cons (get-in values [k 0]) (conj [r] (get-in values [2]))))
+               (into [] (cons (get-in @current-values [k 0]) (conj [r] (get-in @current-values [k 2]))))
                (= v 2)
-               (into [] (cons (get-in values [k 0]) (conj (get-in values [k 1]) [r]))))))
+               (into [] (cons (get-in @current-values [2 0]) (conj [(get-in @current-values [2 1])] 2)))))
+  ([values [r d] f] (cond
+                      (= d 0)
+                      (conj [] (get values 0) (get values 1) (replace-func (add-logic [r d]) f))
+                      (= d 1)
+                      (conj [] (get values 0) (replace-func (add-logic [r d]) f) (get values 1))
+                      (= d 2)
+                      (conj [] (get values 0) (get values 1) (replace-func (add-logic [r d]) f))))
 
-(defn add-elems [values]
-  (cond
-    (= 3 (.length values))
-    (let [n (get-in values [0 1])]
-      ;(replace {n 1.0} (get values 0))
-      (do
-        (replace-func (get values 0))
-        (replace-func (add-logic [0 1]) 2)))
+  ([values [r d] [t g] f] (cond
+                            (= t 0)
+                            (conj [] (replace-func [t g] f) (get values 1) (get values 2))
+                            (= t 1)
+                            (conj [] (get values 0) (replace-func [t g] f) (get values 1))
+                            (= t 2)
+                            (conj [] (get values 0) (get values 1) (replace-func [t g] f)))))
 
-    :else
-    (println "Operations only supports 3*3 matrices")))
+(defn add-loop []
+  ;(replace {n (replace-func (add-logic [0 1]) 2)} orig)
+  (let [current-loc (atom [0 1])]
+    (do
+      (reset!
+        current-values
+        (conj [] (replace-func @current-values) (get @current-values 1) (get @current-values 2)))
+      (reset! current-values (replace-func @current-values @current-loc 2.0))
+      (replace-func @current-values [2 2] (add-logic [2 2]) 3.0)
+      )))
 
 (defn magic-k [values]
   (let [n (.length values)]
