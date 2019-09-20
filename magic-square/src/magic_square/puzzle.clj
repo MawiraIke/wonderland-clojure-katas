f11c76d68ae0aa5851f37c57d25ac85de0493a8b
MawiraIke <mawiraike@gmail.com>
2019-09-20 10:00:25 +0300

    Edit first value

--- magic-square/src/magic_square/puzzle.clj
+++ magic-square/src/magic_square/puzzle.clj
@@ -5,6 +5,9 @@
              [2.5 3.0 3.5]
              [4.0 4.5 5.0]])
 
+(def current-values (atom [[0 0 0]
+                           [0 0 0]
+                           [0 0 0]]))
 
 (defn magic-square [values]
   [[1.0 1.5 2.0]
@@ -37,6 +40,15 @@
   (let [num-a (get current-loc 0)
         num-b (get current-loc 1)]
     [(add-a num-a) (add-b num-b)]))
+
+(defn add-elem-1 [values]
+  (cond
+    (= 3 (.length values))
+    (let [n (get-in values [0 1])]
+      (replace {n 1.0} (get values 0)))
+
+    :else
+    (println "Operations only supports 3*3 matrices")))
 
 (defn magic-k [values]
   (let [n (.length values)]
