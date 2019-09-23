a3f0d865672ff0965b76bd485e542348139622dc
MawiraIke <mawiraike@gmail.com>
2019-09-23 12:28:05 +0300

    Before solution
    Signed-off-by: MawiraIkemawiraike@gmail.com <mawiraike@gmail.com>

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -3,6 +3,10 @@
 (defn get-index [character tensor]
   (map #(.indexOf % character) tensor))
 
+(def maze [[:S 0 1]
+           [1 0 1]
+           [1 0 :E]])
+
 (defn solve-maze [maze]
   (let [start (get-index :S maze)
         end (get-index :E maze)
@@ -13,12 +17,14 @@
         index-of-E (get key-of-E 0)]
     ))
 
-(defn jumper [a b]
+(defn jumper [[a b]]
   "Jumps to the next location"
-  ())
+  (let [n1 (get-index :S maze)
+        n2 (get-index :E maze)
+        n4 0                                                ;Current location
+        ]
+    [n1 n2 n4]))
 
 
-(def maze [[:S 0 1]
-           [1 0 1]
-           [1 0 :E]])
+
 
